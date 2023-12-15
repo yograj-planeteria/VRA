d75d39815a62a226620d4278a404d6d0efcdada2
yograj@planeteria.com <yograj@planeteria.com>
2023-12-15 13:03:59 +0530

    add VRA
    add VRA

---
+++ js/owlmain.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+$('.owl-general .owl-carousel').owlCarousel({
+
+    loop: true,
+	dots:true,
+	autoplay:true,
+    margin: 10,
+    nav: true,
+	 autoWidth:true,
+	 items: 2,
+    navText: [
+        '<img src="/img/white-arrow-left.svg">',
+        '<img src="/img/white-arrow-right.svg">'
+    ],
+    navContainer: '.owl-general .custom-nav',
+    /*responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1,
+			    stagePadding: 200
+        }
+    }*/
+});
+
+$('.slider_area  .owl-carousel').owlCarousel({
+
+    loop: true,
+	dots:true,
+	autoplay:false,
+    margin: 10,
+    nav: false,
+	items: 3,
+    navText: [
+        '<img src="/img/arrow-back-red.svg">',
+        '<img src="/img/arrow-back-red.svg">'
+    ],
+    navContainer: '.LatestNews    .nav-custom',
+  responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 3,
+			 
+        }
+    }
+});
+	
+	
+	$('.logo_slider  .owl-carousel').owlCarousel({
+
+    loop: true,
+	dots:true,
+	autoplay:false,
+    margin: 10,
+    nav: false,
+	items: 3,
+    navText: [
+        '<img src="/img/arrow-back-red.svg">',
+        '<img src="/img/arrow-back-red.svg">'
+    ],
+    navContainer: '.LatestNews    .nav-custom',
+  responsive:{
+        0:{
+            items: 2
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 5,
+			 
+        }
+    }
+});
+	
+
+});
